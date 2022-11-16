ce3c5fd2f14021bf990fae38575e29e7c24b7c3b
mzz2017 <mzz@tuta.io>
2022-11-17 04:47:43 +0800

    feat(http protocol): use GET instead of CONNECT for http-cleartext request

--- protocol/http/http.go
+++ protocol/http/http.go
@@ -1,12 +1,9 @@
 package http
 
 import (
-	"bufio"
 	"crypto/tls"
-	"fmt"
 	"golang.org/x/net/proxy"
 	"net"
-	"net/http"
 	"net/url"
 )
 
@@ -51,46 +48,5 @@ func (s *HttpProxy) Dial(network, addr string) (net.Conn, error) {
 	if s.TlsConfig != nil {
 		c = tls.Client(c, s.TlsConfig)
 	}
-	// HACK. http.ReadRequest also does this.
-	reqURL, err := url.Parse("http://" + addr)
-	if err != nil {
-		c.Close()
-		return nil, err
-	}
-	reqURL.Scheme = ""
-
-	req, err := http.NewRequest("CONNECT", reqURL.String(), nil)
-	if err != nil {
-		c.Close()
-		return nil, err
-	}
-	req.Close = false
-	if s.HaveAuth {
-		req.SetBasicAuth(s.Username, s.Password)
-		req.Header.Set("Proxy-Authorization", req.Header.Get("Authorization"))
-		req.Header.Del("Authorization")
-	}
-
-	err = req.Write(c)
-	if err != nil {
-		c.Close()
-		return nil, err
-	}
-
-	resp, err := http.ReadResponse(bufio.NewReader(c), req)
-	if err != nil {
-		if resp != nil {
-			resp.Body.Close()
-		}
-		c.Close()
-		return nil, err
-	}
-	resp.Body.Close()
-	if resp.StatusCode != 200 {
-		c.Close()
-		err = fmt.Errorf("Connect server using proxy error, StatusCode [%d]", resp.StatusCode)
-		return nil, err
-	}
-
-	return c, nil
+	return NewConn(c, s, addr), nil
 }
