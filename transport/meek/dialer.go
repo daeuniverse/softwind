39b86b36e19ed1be363cb48e309b1a2bf5a86a40
AkinoKaede <i@akinokae.de>
2023-08-05 18:17:38 +0800

    feat: add Meek Transport Dialer

---
+++ transport/meek/dialer.go
@@ -0,0 +1,72 @@
+package meek
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+
+	"github.com/daeuniverse/softwind/netproxy"
+)
+
+type Dialer struct {
+	nextDialer netproxy.Dialer
+	addr       string
+	url        string
+}
+
+func NewDialer(s string, d netproxy.Dialer) (*Dialer, error) {
+	u, err := url.Parse(s)
+	if err != nil {
+		return nil, fmt.Errorf("NewMeek: %w", err)
+	}
+
+	m := &Dialer{
+		nextDialer: d,
+		addr:       u.Host,
+	}
+
+	query := u.Query()
+	m.url = query.Get("url")
+	if m.url == "" {
+		return nil, fmt.Errorf("NewMeek: url is empty")
+	}
+
+	return m, nil
+}
+
+func (m *Dialer) Dial(network, addr string) (c netproxy.Conn, err error) {
+	magicNetwork, err := netproxy.ParseMagicNetwork(network)
+	if err != nil {
+		return nil, err
+	}
+	switch magicNetwork.Network {
+	case "tcp":
+		tripper := &httpTripperClient{
+			nextDialer: m.nextDialer,
+			addr:       m.addr,
+			url:        m.url,
+		}
+
+		clientConfig := &config{
+			MaxWriteSize:             65536,
+			WaitSubsequentWriteMs:    10,
+			InitialPollingIntervalMs: 100,
+			MaxPollingIntervalMs:     1000,
+			MinPollingIntervalMs:     10,
+			BackoffFactor:            1.5,
+			FailedRetryIntervalMs:    1000,
+		}
+
+		assembler := newAssemblerClient(tripper, clientConfig)
+		session, err := assembler.NewSession(context.Background())
+		if err != nil {
+			return nil, err
+		}
+
+		return session.(netproxy.Conn), nil
+	case "udp":
+		return nil, fmt.Errorf("%w: meek+udp", netproxy.UnsupportedTunnelTypeError)
+	default:
+		return nil, fmt.Errorf("%w: %v", netproxy.UnsupportedTunnelTypeError, network)
+	}
+}
---
+++ transport/meek/httprt.go
@@ -0,0 +1,55 @@
+package meek
+
+import (
+	"bytes"
+	"context"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+
+	"github.com/daeuniverse/softwind/netproxy"
+)
+
+type httpTripperClient struct {
+	addr       string
+	nextDialer netproxy.Dialer
+
+	url string
+}
+
+func (c *httpTripperClient) RoundTrip(ctx context.Context, req Request) (resp Response, err error) {
+	connectionTagStr := base64.RawURLEncoding.EncodeToString(req.ConnectionTag)
+
+	httpRequest, err := http.NewRequest("POST", c.url, bytes.NewReader(req.Data))
+	if err != nil {
+		return
+	}
+	httpRequest.Header.Set("X-Session-ID", connectionTagStr)
+
+	transport := &http.Transport{
+		DialContext: func(_ context.Context, network, addr string) (net.Conn, error) {
+			rc, err := c.nextDialer.Dial(network, addr)
+			if err != nil {
+				return nil, fmt.Errorf("[Meek]: dial to %s: %w", c.addr, err)
+			}
+			return &netproxy.FakeNetConn{
+				Conn:  rc,
+				LAddr: nil,
+				RAddr: nil,
+			}, nil
+		},
+	}
+	httpResp, err := transport.RoundTrip(httpRequest)
+	if err != nil {
+		return
+	}
+	defer httpResp.Body.Close()
+
+	result, err := io.ReadAll(httpResp.Body)
+	if err != nil {
+		return
+	}
+	return Response{Data: result}, err
+}
