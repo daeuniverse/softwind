e568984b45946a4daef2c7bbc2dc2366aa0bb4a7
mzz2017 <2017@duck.com>
2023-10-22 01:19:21 +0800

    test

--- pool/bytes_buffer.go
+++ pool/bytes_buffer.go
@@ -3,6 +3,8 @@ package pool
 import (
 	"bytes"
 	"sync"
+
+	bytes2 "github.com/daeuniverse/softwind/pool/bytes"
 )
 
 var bufferPool = sync.Pool{New: func() any { return &bytes.Buffer{} }}
@@ -14,4 +16,15 @@ func GetBuffer() *bytes.Buffer {
 func PutBuffer(buf *bytes.Buffer) {
 	buf.Reset()
 	bufferPool.Put(buf)
+}
+
+var bufferPool2 = sync.Pool{New: func() any { return bytes2.NewBuffer(nil) }}
+
+func GetBuffer2() *bytes2.Buffer {
+	return bufferPool2.Get().(*bytes2.Buffer)
+}
+
+func PutBuffer2(buf *bytes2.Buffer) {
+	buf.Reset()
+	bufferPool2.Put(buf)
 }
